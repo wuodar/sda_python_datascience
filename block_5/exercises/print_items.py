8690214bbd8737bd4ed8d28c3c4aee996f48334d
Wiking92 <dawweber92@gmail.com>
2022-05-21 11:20:03 +0200

    Zadanie pets - w trakcie

--- block_5/exercises/print_items.py
+++ block_5/exercises/print_items.py
@@ -8,8 +8,19 @@ Oraz w każdej kolejnej linii imię jego zwierząt.
 *args - reprezentowane przez klasę tupla - co za tym idzie, możemy sobie do tupli skonwertować
 na przykład listę.
 """
-
+first_name = input("Podaj imię: ")
+last_name = input("Podaj nazwisko: ")
+pets = [input("Podaj imię swojego zwierzaka, wciśnij 0 aby przerwać: ")]
 
 # nagłówek ma zostać tak jak teraz
 def print_name_and_pets(first_name, last_name, *pets):
+    print(first_name, " ", last_name)
+    pets = [print(pet) for pet in pets]
+    while True:
+        if pets == "0":
+            break
+
+
     pass
+
+print_name_and_pets(first_name, last_name, pets)
