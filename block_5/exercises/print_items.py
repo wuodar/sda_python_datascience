93b53d00ab1c207c4fdfb3d08171ed6774ae1b96
Kacper Włodarczyk <kac.wlodarczyk@gmail.com>
2022-05-21 09:51:07 +0200

    pets

---
+++ block_5/exercises/print_items.py
@@ -0,0 +1,15 @@
+"""
+Napisz program, który będzie przyjmowała od użytkownika imię i nazwisko,
+A następnie, do momemntu kiedy uzytkownik nie wprowadzi wartości "0", pobiera imiona
+jego zwierząt (input), a następnie napisz funkcję która będzie jako parametr przyjmowałą to imię i nazwisko,
+oraz dowolną liczbę nazw zwierząt, które dana osoba posiada. Funkcja ma wyprintować imię osoby
+Oraz w każdej kolejnej linii imię jego zwierząt.
+
+*args - reprezentowane przez klasę tupla - co za tym idzie, możemy sobie do tupli skonwertować
+na przykład listę.
+"""
+
+
+# nagłówek ma zostać tak jak teraz
+def print_name_and_pets(first_name, last_name, *pets):
+    pass
