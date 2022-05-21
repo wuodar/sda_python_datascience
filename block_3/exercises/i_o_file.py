ef63cb7c7ef5d7d9f036c06d5e420f57694884b4
Michal <robakmichal@onet.pl>
2022-05-21 10:09:53 +0200

    Test commit & push to my branch

--- block_3/exercises/i_o_file.py
+++ block_3/exercises/i_o_file.py
@@ -6,3 +6,9 @@ rest_of_file = f.read()
 print(line)
 print(rest_of_file)
 
+"""
+Napisz funkcje, która wczyta dwie ostatnie linie w pliku .txt i wyswietli je na ekranie
+(użyj funkcji seek)
+
+def read_last_2_lines(filename):
+    with open(filename) as f:
