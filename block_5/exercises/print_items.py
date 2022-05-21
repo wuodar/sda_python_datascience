e5d26d50da9b9e36add65a0bf57c2eb4aa149188
Wiking92 <dawweber92@gmail.com>
2022-05-21 11:51:13 +0200

    Zadanie pets - zrobione

--- block_5/exercises/print_items.py
+++ block_5/exercises/print_items.py
@@ -4,12 +4,24 @@ A następnie, do momemntu kiedy uzytkownik nie wprowadzi wartości "0", pobiera 
 jego zwierząt (input), a następnie napisz funkcję która będzie jako parametr przyjmowałą to imię i nazwisko,
 oraz dowolną liczbę nazw zwierząt, które dana osoba posiada. Funkcja ma wyprintować imię osoby
 Oraz w każdej kolejnej linii imię jego zwierząt.
-
 *args - reprezentowane przez klasę tupla - co za tym idzie, możemy sobie do tupli skonwertować
 na przykład listę.
 """
-
+first_name = input("Podaj imię: ")
+last_name = input("Podaj nazwisko: ")
+pets_list = []
 
 # nagłówek ma zostać tak jak teraz
 def print_name_and_pets(first_name, last_name, *pets):
-    pass
+        print(first_name, " ", last_name)
+        for pet in pets_list:
+            print(pet)
+
+
+while True:
+    pets = input("Podaj imię swojego zwierzaka, wciśnij 0 aby przerwać: ")
+    pets_list.append(pets)
+    if pets == "0":
+        break
+
+print_name_and_pets(first_name, last_name, pets_list)
