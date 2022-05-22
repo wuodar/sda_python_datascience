726a0c82281afea0e38a9580ad62f8368668a53f
Wiking92 <dawweber92@gmail.com>
2022-05-22 10:03:41 +0200

    Zrobione

--- block_5/exercises/function_operator.py
+++ block_5/exercises/function_operator.py
@@ -5,3 +5,31 @@ Następnie w funkcji sprawdź, jaki operator został podany, i zwróc odpowiedni
 sumę/różnicę/iloraz/iloczyn
 przekazanych liczb w zmiennej numbers (wykorzystaj do tego celu if/elif)
 """
+
+operator = input("Podaj operator (+, -, /, *): ")
+count = int(input("Podaj liczbę cyfr do wykonania operacji: "))
+
+number_list = []
+for i in range(count):
+    number = int(input("Podaj liczbę: "))
+    number_list.append(number)
+
+def function_operator(operator, *numbers):
+    print("Podano operator: ", operator)
+    print(number_list)
+    result = 0
+    for number in number_list:
+        if operator == "+":
+            result = result + number
+        elif operator == "-":
+            number -= number
+        elif operator == "*":
+            number *= number
+        elif operator == "/":
+            number /= number
+
+    return number
+
+print(function_operator(operator, number_list))
+
+
