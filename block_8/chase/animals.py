2f0d148f2e82d825c510244b6a580a3f1c4814a8
Wiking92 <dawweber92@gmail.com>
2022-05-25 20:02:15 +0200

    Powrótka z zadań 1.0

--- block_8/chase/animals.py
+++ block_8/chase/animals.py
@@ -14,13 +14,22 @@ class Animal:
         return self.x_pos, self.y_pos
 
     def move(self):
-        # random.choice pobiera jako parametr sekwencje (lista/tupla) i zwraca losowy element
-        pass
+        self.x_pos = random.choice([-1, 0, 1])
+        self.y_pos = random.choice([-1, 0, 1])
+        # random.choice pobiera jako parametr sekwencje (lista/tupla) i zwraca losowy element v
+        # pass
 
 
 class Sheep(Animal):
     def __init__(self, init_pos_limit: float, move_dist: float):
-        pass
+        self.x_pos = random.uniform(-init_pos_limit, init_pos_limit)
+        self.y_pos = random.uniform(-init_pos_limit, init_pos_limit)
+        self.move_dist = move_dist
+        self.alive = True
+
+    def move(self):
+        self.x_pos = self.move_dist * random.choice([-1, 0, 1])
+        self.y_pos = self.move_dist * random.choice([-1, 0, 1])
 
 
 class Wolf(Animal):
@@ -28,12 +37,34 @@ class Wolf(Animal):
         super(Wolf, self).__init__(0, 0, move_dist)
         self.eaten_sheeps = 0
 
+    def move(self):
+        self.x_pos = self.move_dist * random.choice([-1, 0, 1])
+        self.y_pos = self.move_dist * random.choice([-1, 0, 1])
+
     def __get_closest_sheep(self, sheeps: List[Sheep]) -> Tuple[int, float]:
         """
         returns index of the closest sheep, and the distance of the sheep
         """
-        pass
+        distance = []
+        distance_wolf = self.cords
+        for sheep in sheeps:
+            distance.append(dist(sheep.cords, distance_wolf))
 
-    def chase(self, sheeps: List[Sheep]):
-        pass
+        index = distance.index(min(distance))
+        return index, min(distance)
+
+
+    def chase(self, sheeps: List[Sheep]) -> Optional[Sheep]:
+        closest_sheep = self.__get_closest_sheep(sheeps)
+        if closest_sheep[1] > self.move_dist:
+            self.move()
+        else:
+            sheep_cords = sheeps[closest_sheep[0]]
+            self.x_pos = sheep_cords.x_pos
+            self.y_pos = sheep_cords.y_pos
+            sheeps[closest_sheep[0]].alive = False
+            self.eaten_sheeps += 1
+
+            return closest_sheep
+            #return sheeps[closest_sheep[0]]
 
--- block_8/chase/simulation.py
+++ block_8/chase/simulation.py
@@ -2,20 +2,38 @@ from typing import Optional, List
 import logging
 from animals import Sheep, Wolf
 
-
 logging.basicConfig(level=logging.INFO)
 
 
 def count_alive_sheeps(sheeps: List[Sheep]) -> int:
-    pass
+    count_alive = 0
+    for sheep in sheeps:
+        if sheep.alive:
+            count_alive += 1
+
+    return count_alive
+    # pass
 
 
 def move_sheeps(sheeps: List[Sheep]) -> None:
-    pass
+    for sheep in sheeps:
+        if sheep.alive:
+            sheep.move()
 
 
 def run_simulation(iterations_count: int, wolf: Wolf, sheeps: List[Sheep]):
-    pass
+    for i in range(iterations_count):
+        move_sheeps(sheeps)
+        wolf_chase_result = wolf.chase(sheeps)
+        if wolf_chase_result is not None:
+            print("Wolf ate sheep number {}".format(wolf_chase_result[0]))
+
+        if count_alive_sheeps(sheeps) == 0:
+            print("Simulation finished! There are {} alive sheeps!".format(count_alive_sheeps(sheeps)))
+            break
+
+    print("Simulation finished! There are {} alive sheeps!".format(count_alive_sheeps(sheeps)))
+
 
 if __name__ == "__main__":
     sheeps = [Sheep(10, 7) for i in range(20)]
