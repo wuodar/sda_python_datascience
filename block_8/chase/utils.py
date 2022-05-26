3dc104a13774d1a82566d27ca7ebd0529833dd21
Kacper Włodarczyk <kac.wlodarczyk@gmail.com>
2022-05-26 18:15:10 +0200

    dec

--- block_8/chase/utils.py
+++ block_8/chase/utils.py
@@ -1,5 +1,18 @@
 from time import time
 
+# zwykły dekorator
+def timer(func):
+    def wrapper(*args, **kwargs):
+        tik = time()
+        res = func(*args, **kwargs)
+        tok = time()
+        execution_time = tok - tik
+        print(f"Function exeuction has taken {execution_time} seconds.")
+        return res
+
+    return wrapper
+
+# dekorator z dodatkowym parametrem przy wywołaniu (przykład użycia w linii 33)
 def timer_with_param(some_value):
     def timer(func):
         def wrapper(*args, **kwargs):
