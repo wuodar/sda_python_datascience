8207e88dd188dc50e9ad4ae6d568ed89e97cae72
Kacper Włodarczyk <kac.wlodarczyk@gmail.com>
2022-05-26 17:23:33 +0200

    dekoarator 1

---
+++ block_9/decorator_1.py
@@ -0,0 +1,31 @@
+"""
+Napisz program pobierający od użytkownika dowolny input.
+Pobrany input będzie parametrem do funkcji
+    def print_reversed(input_str: str) -> None:
+Funkcja ma wyprintować wejściowego stringa w odwrotnej kolejności
+(np. input_str="abcde", wtedy funkcja wyprintuje "edcba").
+Dodatkowo, napisz dekorator
+    def make_pretty(func):
+który będzie printował linię "===================="
+przed i po wykonaniu się funkcji print_reversed.
+Po udekorowaniu funkcji print_reversed dekoratorem make_pretty, przykładowym outputem z wykonania się programu,
+dla inputu od użytkownika "abecadło", będzie następująca wartość:
+    "===================="
+    "ołdaceba"
+    "===================="
+"""
+
+def make_pretty(func):
+    pass
+
+
+@make_pretty
+def print_reversed(input_str: str) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    input_str = input("Podaj dowolną wartość: ")
+
+    print_reversed(input_str)
+
