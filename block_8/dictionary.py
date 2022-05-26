78f0fbd2e5e035bc5cd40e4955b3c22897672010
Maciej Szkudlarek <szkudlarekmaciej@outlook.com>
2022-05-26 17:35:46 +0200

    dict.py changed.

--- block_8/dictionary.py
+++ block_8/dictionary.py
@@ -1,4 +1,5 @@
 from typing import Dict, List, Any
+from collections import Counter
 """
 1)
 Napisz funkcję, która pobierze od użytkownika dowolny input, a następnie stworzy z niego słownik.
@@ -20,16 +21,49 @@ Klucze mają być posortowane, od najmniejszego do największego (względem wart
 
 
 def create_dict(input_string: str) -> Dict[str, int]:
-    pass
+    dictionary = dict()
+    for i in range(len(input_string)):
+        dictionary[input_string[i]] = i+1
+
+    return dictionary
+
+def create_dict2(input_string: str) -> Dict[str, int]:
+    dictionary = dict()
+    for i, v in enumerate(input_string):
+        dictionary[v] = i + 1
+
+    return dictionary
+
+def create_dict3(input_string: str) -> Dict[str, int]:
+    dictionary = {value: index+1 for index, value in enumerate(input_string)}
+
+    return dictionary
 
 # Any oznacza dowolny typ
-def get_keys_with_largest_values(input_dict: Dict[Any, float]) -> List[Any]:
-    pass
+def get_keys_with_largest_values(input_dict: Dict[Any, int]) -> List[Any]:
+    d = Counter(input_dict).most_common(3)
+    return [x[0] for x in d][::-1]
 
+def get_keys_with_largest_values2(input_dict: Dict[Any, int]) -> List[Any]:
+    sorted_values = sorted(input_dict.values())
+    sorted_dict = dict()
+
+    for val in sorted_values:
+        for key in input_dict:
+            if input_dict[key] == val:
+                sorted_dict[key] = input_dict[key]
+                break
+    
+    sorted_keys = list(sorted_dict.keys())
+    return sorted_keys[-3:]
+ 
 
 if __name__ == "__main__":
-    input_str = input("1) Podaj dowolnego stringa: ")
+    #input_str = input("1) Podaj dowolnego stringa: ")
+    input_str = "kajakarz"
     output_dict = create_dict(input_str)
     print(f"Zadanie 1, wynik: {output_dict}")
     print(f"Zadanie 2, wynik: {get_keys_with_largest_values(output_dict)}")
+
+    #print(get_keys_with_largest_values2(output_dict))
 
