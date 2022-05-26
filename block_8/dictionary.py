1af9659cd85ed3b410469402168a0a90cc4d976d
Wiking92 <dawweber92@gmail.com>
2022-05-26 17:36:41 +0200

    Dictionary - zadanie

--- block_8/dictionary.py
+++ block_8/dictionary.py
@@ -20,16 +20,34 @@ Klucze mają być posortowane, od najmniejszego do największego (względem wart
 
 
 def create_dict(input_string: str) -> Dict[str, int]:
-    pass
+    dict = {}
+    count = 0
+    for character in input_string:
+        count += 1
+        dict[character] = count
+
+    return dict
 
 # Any oznacza dowolny typ
 def get_keys_with_largest_values(input_dict: Dict[Any, float]) -> List[Any]:
-    pass
+    max_list = []
+    for digit in input_dict:
+        max_digit = max(input_dict)
+        input_dict.remove
+        max_list.append(max_digit)
+
+    return max_digit
+
+
+
+
+    return max_list
+
 
 
 if __name__ == "__main__":
     input_str = input("1) Podaj dowolnego stringa: ")
     output_dict = create_dict(input_str)
     print(f"Zadanie 1, wynik: {output_dict}")
-    print(f"Zadanie 2, wynik: {get_keys_with_largest_values(output_dict)}")
+    print(f"Zadanie 2, wynik: {get_keys_with_largest_values([1.0, 2.0, 2.3, 5.0, 11.0, 2.2])}")
 
