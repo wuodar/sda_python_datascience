fac6ae1ed8936b932701bc3fb8c4d1005ed594aa
Kacper Włodarczyk <kac.wlodarczyk@gmail.com>
2022-05-26 19:02:32 +0200

    decorators

--- block_9/decorator_2.py
+++ block_9/decorator_2.py
@@ -8,17 +8,37 @@ który sprawdz, czy parametr b nie jest równy 0 (nie dzielimy przez 0!).
 Jeśli tak, wyprintuj "Whoops! cannot divide" i zwróć z funkcji wartość None.
 """
 
+# rozwiazanie przy pomocy *args, **kwargs
+# def smart_divide(func):
+#     def wrapper(*args, **kwargs):
+#         if len(args) == 2:
+#             b = args[1]
+#         elif 'b' in kwargs.keys():
+#             b = kwargs['b']
+#         else:
+#             raise TypeError("Missing reuqired positional argument: 'b'")
+#         if b == 0:
+#             print("Whoops! cannot divide")
+#             return None
+#         return func(a, b)
+#     return wrapper
+
 
 def smart_divide(func):
-    pass
+    def wrapper(a, b):
+        if b == 0:
+            print("Whoops! cannot divide")
+            return None
+        return func(a, b)
+    return wrapper
 
 
 @smart_divide
 def divide(a: float, b: float) -> float:
-    pass
+    return a / b
 
 
 if __name__ == "__main__":
     a = 3
     b = 0
-    res = divide(a, b)
+    res = divide(a=a)
